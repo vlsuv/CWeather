e6f4d7dd6f6cbcc3cd99cb325481efdf1909f070
Vlad Suchkov <vsuchkov96@gmail.com>
2021-02-04 18:22:51 +0400

    add current weather page

---
+++ CWeather/Controllers/CurrentWeatherController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CWeather
+//
+//  Created by vlsuv on 04.02.2021.
+//  Copyright © 2021 vlsuv. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var feelLikeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var locationNameLabel: UILabel!
+    
+    // MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Handlers
+
+
+}
+
